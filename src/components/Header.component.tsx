3f80178c74b3409e9e05a197f5bf7a785a92be40
desteban <esteban28c@hotmail.com>
2020-11-09 22:30:11 -0500

    creacion del header

---
+++ src/components/Header.component.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function HeaderComponent() {
+  return (
+    <nav className="nav-wrapper verde">
+      <span className="brand-logo center">ChatUnicundi</span>
+    </nav>
+  );
+}
+
+export default HeaderComponent;
